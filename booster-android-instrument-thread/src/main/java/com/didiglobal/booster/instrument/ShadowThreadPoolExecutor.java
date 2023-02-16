0313de3517c78e3c82c29da751536cb246ae9a1f
Daniel Olsson <daniel@degoo.com>
2023-02-16 09:37:47 +0100

    Return our executor in more places

--- booster-android-instrument-thread/src/main/java/com/didiglobal/booster/instrument/ShadowThreadPoolExecutor.java
+++ booster-android-instrument-thread/src/main/java/com/didiglobal/booster/instrument/ShadowThreadPoolExecutor.java
@@ -19,18 +19,30 @@ public class ShadowThreadPoolExecutor extends ThreadPoolExecutor {
     private boolean isIBThreadPoolExecutor = false;
 
     public static ThreadPoolExecutor newThreadPoolExecutor(final int corePoolSize, final int maxPoolSize, final long keepAliveTime, final TimeUnit unit, final BlockingQueue<Runnable> workQueue, final String name) {
+        if (EXECUTOR != null) {
+            return EXECUTOR;
+        }
         return new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveTime, unit, workQueue, new NamedThreadFactory(name));
     }
 
     public static ThreadPoolExecutor newThreadPoolExecutor(final int corePoolSize, final int maxPoolSize, final long keepAliveTime, final TimeUnit unit, final BlockingQueue<Runnable> workQueue, final ThreadFactory factory, final String name) {
+        if (EXECUTOR != null) {
+            return EXECUTOR;
+        }
         return new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveTime, unit, workQueue, new NamedThreadFactory(factory, name));
     }
 
     public static ThreadPoolExecutor newThreadPoolExecutor(final int corePoolSize, final int maxPoolSize, final long keepAliveTime, final TimeUnit unit, final BlockingQueue<Runnable> workQueue, final RejectedExecutionHandler handler, final String name) {
+        if (EXECUTOR != null) {
+            return EXECUTOR;
+        }
         return new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveTime, unit, workQueue, new NamedThreadFactory(name), handler);
     }
 
     public static ThreadPoolExecutor newThreadPoolExecutor(final int corePoolSize, final int maxPoolSize, final long keepAliveTime, final TimeUnit unit, final BlockingQueue<Runnable> workQueue, final ThreadFactory factory, final RejectedExecutionHandler handler, final String name) {
+        if (EXECUTOR != null) {
+            return EXECUTOR;
+        }
         return new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveTime, unit, workQueue, new NamedThreadFactory(factory, name), handler);
     }
 
@@ -39,24 +51,36 @@ public class ShadowThreadPoolExecutor extends ThreadPoolExecutor {
     //<editor-fold desc="* optimized thread pool executor">
 
     public static ThreadPoolExecutor newOptimizedThreadPoolExecutor(final int corePoolSize, final int maxPoolSize, final long keepAliveTime, final TimeUnit unit, final BlockingQueue<Runnable> workQueue) {
+        if (EXECUTOR != null) {
+            return EXECUTOR;
+        }
         final ThreadPoolExecutor executor = new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveTime, unit, workQueue);
         executor.allowCoreThreadTimeOut(keepAliveTime > 0);
         return executor;
     }
 
     public static ThreadPoolExecutor newOptimizedThreadPoolExecutor(final int corePoolSize, final int maxPoolSize, final long keepAliveTime, final TimeUnit unit, final BlockingQueue<Runnable> workQueue, final ThreadFactory factory) {
+        if (EXECUTOR != null) {
+            return EXECUTOR;
+        }
         final ThreadPoolExecutor executor = new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveTime, unit, workQueue, factory);
         executor.allowCoreThreadTimeOut(keepAliveTime > 0);
         return executor;
     }
 
     public static ThreadPoolExecutor newOptimizedThreadPoolExecutor(final int corePoolSize, final int maxPoolSize, final long keepAliveTime, final TimeUnit unit, final BlockingQueue<Runnable> workQueue, final RejectedExecutionHandler handler) {
+        if (EXECUTOR != null) {
+            return EXECUTOR;
+        }
         final ThreadPoolExecutor executor = new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveTime, unit, workQueue, handler);
         executor.allowCoreThreadTimeOut(keepAliveTime > 0);
         return executor;
     }
 
     public static ThreadPoolExecutor newOptimizedThreadPoolExecutor(final int corePoolSize, final int maxPoolSize, final long keepAliveTime, final TimeUnit unit, final BlockingQueue<Runnable> workQueue, final ThreadFactory factory, final RejectedExecutionHandler handler) {
+        if (EXECUTOR != null) {
+            return EXECUTOR;
+        }
         final ThreadPoolExecutor executor = new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveTime, unit, workQueue, factory, handler);
         executor.allowCoreThreadTimeOut(keepAliveTime > 0);
         return executor;
@@ -67,24 +91,36 @@ public class ShadowThreadPoolExecutor extends ThreadPoolExecutor {
     // <editor-fold desc="* optimized named thread pool executor">
 
     public static ThreadPoolExecutor newOptimizedThreadPoolExecutor(final int corePoolSize, final int maxPoolSize, final long keepAliveTime, final TimeUnit unit, final BlockingQueue<Runnable> workQueue, final String name) {
+        if (EXECUTOR != null) {
+            return EXECUTOR;
+        }
         final ThreadPoolExecutor executor = new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveTime, unit, workQueue, new NamedThreadFactory(name));
         executor.allowCoreThreadTimeOut(keepAliveTime > 0);
         return executor;
     }
 
     public static ThreadPoolExecutor newOptimizedThreadPoolExecutor(final int corePoolSize, final int maxPoolSize, final long keepAliveTime, final TimeUnit unit, final BlockingQueue<Runnable> workQueue, final ThreadFactory factory, final String name) {
+        if (EXECUTOR != null) {
+            return EXECUTOR;
+        }
         final ThreadPoolExecutor executor = new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveTime, unit, workQueue, new NamedThreadFactory(factory, name));
         executor.allowCoreThreadTimeOut(keepAliveTime > 0);
         return executor;
     }
 
     public static ThreadPoolExecutor newOptimizedThreadPoolExecutor(final int corePoolSize, final int maxPoolSize, final long keepAliveTime, final TimeUnit unit, final BlockingQueue<Runnable> workQueue, final RejectedExecutionHandler handler, final String name) {
+        if (EXECUTOR != null) {
+            return EXECUTOR;
+        }
         final ThreadPoolExecutor executor = new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveTime, unit, workQueue, new NamedThreadFactory(name), handler);
         executor.allowCoreThreadTimeOut(keepAliveTime > 0);
         return executor;
     }
 
     public static ThreadPoolExecutor newOptimizedThreadPoolExecutor(final int corePoolSize, final int maxPoolSize, final long keepAliveTime, final TimeUnit unit, final BlockingQueue<Runnable> workQueue, final ThreadFactory factory, final RejectedExecutionHandler handler, final String name) {
+        if (EXECUTOR != null) {
+            return EXECUTOR;
+        }
         final ThreadPoolExecutor executor = new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveTime, unit, workQueue, new NamedThreadFactory(factory, name), handler);
         executor.allowCoreThreadTimeOut(keepAliveTime > 0);
         return executor;
