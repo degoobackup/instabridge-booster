0b2d7d1967ccb33765204f256ce185a114a04483
Artem Chepurnyi <mail@artemchep.com>
2024-06-20 20:38:27 +0300

    improvement: Bring back shared preferences

---
+++ booster-transform-shared-preferences/src/main/kotlin/com/didiglobal/booster/transform/sharedpreferences/SharedPreferencesVariantProcessor.kt
@@ -0,0 +1,25 @@
+package com.didiglobal.booster.transform.sharedpreferences
+
+import com.android.build.api.variant.DynamicFeatureVariantBuilder
+import com.android.build.api.variant.LibraryVariantBuilder
+import com.android.build.api.variant.VariantBuilder
+import com.didiglobal.booster.task.spi.VariantProcessor
+import com.didiglobal.booster.transform.shared.preferences.Build
+import com.didiglobal.booster.transform.shared.preferences.Build.GROUP
+import com.didiglobal.booster.transform.shared.preferences.Build.VERSION
+import com.google.auto.service.AutoService
+import org.gradle.api.Project
+
+/**
+ * @author neighbWang
+ */
+@AutoService(VariantProcessor::class)
+class SharedPreferencesVariantProcessor(private val project: Project) : VariantProcessor {
+
+    override fun beforeProcess(variantBuilder: VariantBuilder) {
+        if (variantBuilder is LibraryVariantBuilder || variantBuilder is DynamicFeatureVariantBuilder) {
+            return
+        }
+        project.dependencies.add("${variantBuilder.name}Implementation", "com.github.degoobackup.instabridge-booster:booster-android-instrument-shared-preferences:${VERSION}")
+    }
+}
